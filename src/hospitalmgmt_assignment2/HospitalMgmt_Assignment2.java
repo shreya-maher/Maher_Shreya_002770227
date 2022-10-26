fe7edc3dca60101a18d4b375b4ef8eaefe1341d7
shreya-maher <maher@10.0.0.53>
2022-10-26 00:33:16 -0400

    Java Assignment 2: Healthcare Management System Modules Included: Homepage and Login Pages

---
+++ src/hospitalmgmt_assignment2/HospitalMgmt_Assignment2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hospitalmgmt_assignment2;
+
+/**
+ *
+ * @author maher
+ */
+public class HospitalMgmt_Assignment2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
